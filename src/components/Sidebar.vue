d1310405d9887dbb843cace28a4d13d7b50b67ef
Brian Mason <brian.kw.mason@gmail.com>
2023-06-22 14:35:01 -0400

    Added proper coloring and structure. Numbers and extra lines still not working properly

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,16 +5,37 @@
     </div>
     <div class="scroll">
       <div class="entry" v-for="(entry, index) in requests" :key="index">
-        <div class="route" v-if="entry.path">Route {{ entry.path }}</div>
+        <div class="row">
+          <p class="number">1</p>
+          <div class="route" v-if="entry.path">Route {{ entry.path }}</div>
+        </div>
+
         <div v-if="entry.displayURL">
-          <div>> {{ entry.method.toUpperCase() }} \</div>
-          <div>'{{ entry.displayURL }}' \</div>
-          <div class="param">-H “Authorization: api-key API_KEY"</div>
-          <div class="param" v-for="(value, name) in entry.params" :key="name">
-            -d {{ name }}={{ value }}
+          <div class="row">
+            <p class="number">1</p>
+            <div class="request-type">> {{ entry.method.toUpperCase() }} \</div>
           </div>
-          <div class="param" v-if="entry.data">
-            -d '{{ JSON.stringify(entry.data) }}'
+          <div class="row">
+            <p class="number">1</p>
+            <div class="url">'{{ entry.displayURL }}' \</div>
+          </div>
+          <div class="row">
+            <p class="number">1</p>
+            <div class="param">-H “Authorization: api-key API_KEY"</div>
+          </div>
+          <div class="row">
+            <p class="number">1</p>
+            <div
+              class="param"
+              v-for="(value, name) in entry.params"
+              :key="name"
+            >
+              -d {{ name }}={{ value }}
+            </div>
+          </div>
+          <div class="row" v-if="entry.data">
+            <p class="number">1</p>
+            <div class="param">-d '{{ JSON.stringify(entry.data) }}'</div>
           </div>
         </div>
       </div>
@@ -36,12 +57,14 @@ export default {
 <style scoped lang="scss">
 .sidebar {
   position: relative;
-  background: var(--dark, #232320);
+  background: var(--request-log, #232320);
   min-height: 100vh;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
 
-  h4,
-  div {
-    color: white;
+  h4 {
+    color: var(--color, #fff);
+    font-weight: 500;
+    font-size: 18px;
   }
 
   > .status {
@@ -51,10 +74,11 @@ export default {
     padding: 0 24px;
     position: sticky;
     z-index: 1;
+    background: var(--request-log, #232320);
     top: 0;
     left: 0;
     width: 100%;
-    background: var(--dark-accent, rgb(76, 75, 72));
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   }
 
   > .scroll {
@@ -62,27 +86,46 @@ export default {
     box-sizing: border-box;
     padding: 20px 20px 6px;
     overflow: scroll;
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
 
     > .entry {
-      margin: 0 0 20px 0;
+      margin: 0 0 0 0;
       animation: fadein 0.3s;
       line-height: 24px;
 
-      > .route {
-        border: 1px dashed white;
-        border-width: 1px 0 1px 0;
-        padding: 10px 0;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-      }
+      .row {
+        display: flex;
+        align-items: center;
+        .number {
+          margin: 0px;
+          margin-right: 10px;
+          color: #b28f6b;
+          opacity: 0.5;
+        }
+        > .route {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          color: #b28f6b;
+        }
 
-      .param {
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
+        .param {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+        .url {
+          color: #0093c9;
+        }
+        .request-type {
+          color: #ff5c33;
+        }
       }
     }
+  }
+  > .scroll::-webkit-scrollbar {
+    display: none;
   }
 }
 </style>
